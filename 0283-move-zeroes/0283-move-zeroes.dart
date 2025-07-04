d8db658f9ded1150414141405a081864643f991a
mmie <57971918+mmiemmiedevelop@users.noreply.github.com>
2025-07-04 09:12:25 +0900

    Time: 1 ms (100%), Space: 155.6 MB (61.67%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.dart
+++ 0283-move-zeroes/0283-move-zeroes.dart
@@ -1,16 +1,16 @@
 class Solution {
   List<int> moveZeroes(List<int> nums) {
-    int insertPos = 0;
+    int insertPosition = 0;
 
     // 0이 아닌 값을 앞으로 옮긴다
     for (int num in nums) {
       if (num != 0) {
-        nums[insertPos++] = num;
+        nums[insertPosition++] = num;
       }
     }
 
     // 나머지 뒷부분을 0으로 채운다
-    for (int i = insertPos; i < nums.length; i++) {
+    for (int i = insertPosition; i < nums.length; i++) {
       nums[i] = 0;
     }
 
