bcc924a702a06ff7d68219217aef03ad2628d783
mmie <57971918+mmiemmiedevelop@users.noreply.github.com>
2025-07-16 10:05:00 +0900

    Time: 0 ms (100%), Space: 149.6 MB (57.14%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.dart
+++ 0724-find-pivot-index/0724-find-pivot-index.dart
@@ -1,19 +1,20 @@
 class Solution {
   int pivotIndex(List<int> nums) {
-    //if (nums.isEmpty) return -1;
-
-    //int totalSum = nums.reduce((a, b) => a + b);
-    int totalSum = 0; 
-    for (int num in nums) {
-      totalSum += num;
+    int totalSum = 0;
+    for (int i = 0; i < nums.length; i++) {
+      totalSum += nums[i];
     }
+
     int leftSum = 0;
     for (int i = 0; i < nums.length; i++) {
-      int rightSum = totalSum - leftSum - nums[i];
-      if (leftSum == rightSum) {
+      if (i != 0) {
+        leftSum += nums[i - 1];
+      }
+
+      totalSum -= nums[i];
+      if (leftSum == totalSum) {
         return i;
       }
-      leftSum += nums[i];
     }
     return -1;
   }
