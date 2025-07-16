5b98550d32ab379aee0b5b1b0f508fbaa1729c80
mmie <57971918+mmiemmiedevelop@users.noreply.github.com>
2025-07-16 10:10:04 +0900

    Update 0724-find-pivot-index.dart

--- 0724-find-pivot-index/0724-find-pivot-index.dart
+++ 0724-find-pivot-index/0724-find-pivot-index.dart
@@ -1,12 +1,14 @@
+
+//100% 나온코드
 class Solution {
   int pivotIndex(List<int> nums) {
     int totalSum = 0;
-    for (int i = 0; i < nums.length; i++) {
+    for (int i = 0; i < nums.length; i++) {//이부분다름
       totalSum += nums[i];
     }
 
     int leftSum = 0;
-    for (int i = 0; i < nums.length; i++) {
+    for (int i = 0; i < nums.length; i++) {//여기부터도 다름
       if (i != 0) {
         leftSum += nums[i - 1];
       }
@@ -15,6 +17,24 @@ class Solution {
       if (leftSum == totalSum) {
         return i;
       }
+    }
+    return -1;
+  }
+}
+
+//원래코드
+class Solution {
+  int pivotIndex(List<int> nums) {
+   // if (nums.isEmpty) return -1; >> 필요없음! 문제에서 이미 1이상이라고했슴!ㅠㅠ
+
+    int totalSum = nums.reduce((a, b) => a + b);//이부분다름
+    int leftSum = 0;
+    for (int i = 0; i < nums.length; i++) {
+      int rightSum = totalSum - leftSum - nums[i];
+      if (leftSum == rightSum) {
+        return i;
+      }
+      leftSum += nums[i];
     }
     return -1;
   }
