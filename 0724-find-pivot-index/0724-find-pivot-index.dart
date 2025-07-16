06b22623d06f1a967386366f9b7440952606c9d8
mmie <57971918+mmiemmiedevelop@users.noreply.github.com>
2025-07-16 10:03:13 +0900

    Time: 1 ms (85.71%), Space: 149.4 MB (66.67%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.dart
+++ 0724-find-pivot-index/0724-find-pivot-index.dart
@@ -1,8 +1,12 @@
 class Solution {
   int pivotIndex(List<int> nums) {
-   // if (nums.isEmpty) return -1;>> 불필요 문제에서 이미 1이상이라고 주어짐(문제를 잘읽자ㅠ.ㅠ)
+    //if (nums.isEmpty) return -1;
 
-    int totalSum = nums.reduce((a, b) => a + b);
+    //int totalSum = nums.reduce((a, b) => a + b);
+    int totalSum = 0; 
+    for (int num in nums) {
+      totalSum += num;
+    }
     int leftSum = 0;
     for (int i = 0; i < nums.length; i++) {
       int rightSum = totalSum - leftSum - nums[i];
