8ebc3c96afb6980a7d313d2a1ad0e5b7890396a7
mmie <57971918+mmiemmiedevelop@users.noreply.github.com>
2025-08-05 09:17:36 +0900

    Time: 214 ms (5.32%), Space: 158.7 MB (5.32%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.dart
+++ 0125-valid-palindrome/0125-valid-palindrome.dart
@@ -1,13 +1,26 @@
 class Solution {
   bool isPalindrome(String s) {
-    String rm = s.replaceAll(RegExp(r'[^a-zA-Z0-9]'), '');
-    String toLower = rm.toLowerCase();
-
-    for (int i = 0, j = toLower.length - 1; i < j; i++, j--) {
-      if (toLower[i] != toLower[j]) {
-        return false;
+    // 알파벳과 숫자만 남기고, 소문자로 변환
+    String filtered = '';
+    for (var c in s.split('')) {
+      if (RegExp(r'[a-zA-Z0-9]').hasMatch(c)) {
+        filtered += c.toLowerCase();
       }
     }
+    // "A man, a plan, a canal: Panama"
+    // "amanaplanacanalpanama"
+
+    // 양쪽 끝에서부터 비교
+    int left = 0;
+    int right = filtered.length - 1;
+    while (left < right) {
+      if (filtered[left] != filtered[right]) {
+        return false;
+      }
+      left++;
+      right--;
+    }
+
     return true;
   }
 }
